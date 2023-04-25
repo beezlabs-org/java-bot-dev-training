2c60919514edc5398ee431abbef4d9ae016df623
Abishek <jettleetrif@gmail.com>
2023-04-25 13:19:33 +0530

    initial commit

---
+++ src/main/java/com/beezlabs/bots/ReadEmailBot.java
@@ -0,0 +1,69 @@
+package com.beezlabs.bots;
+
+import com.beezlabs.hiveonserver.libs.JavaBotTemplate;
+import com.beezlabs.hiveonserver.libs.TulipService;
+import com.beezlabs.tulip.libs.models.BotExecutionModel;
+import com.beezlabs.tulip.libs.models.BotIdentity;
+
+import java.util.*;
+
+public class ReadEmailBot extends JavaBotTemplate {
+    BotExecutionModel botExecutionModel;
+    @Override
+    protected void botLogic(BotExecutionModel botExecutionModel) {
+        this.botExecutionModel = botExecutionModel;
+        List<String> emails = fetchEmails();
+        Map<String, String> hostCredentials = fetchHostCredentials();
+    }
+
+    private List<String> fetchEmails() {
+
+    }
+
+    private Map<String, String> fetchHostCredentials() throws Exception {
+        info("process to fetch the hostname username and password to send email has been initiated");
+        String genericIdentityKey;
+        Map<String, String> hostUserAndPassword = new HashMap<>();
+        try {
+            info("trying to fetch the the key from inputs to fetch identity from identities");
+            genericIdentityKey = botExecutionModel.getProposedBotInputs().get("genericIdentityKey").getValue().toString();
+            if (genericIdentityKey == null || genericIdentityKey.equals("")) {
+                throw new Exception("because the value for key 'genericIdentityKey' is either null or an empty string");
+            }
+        } catch (NullPointerException nullPointerException) {
+            throw new NullPointerException("because the key 'genericIdentityKey' was not found in bot inputs" + nullPointerException.getMessage());
+        } catch (Exception exception) {
+            throw new Exception("while fetching inputs " + exception.getMessage() + " " + Arrays.toString(exception.getStackTrace()));
+        }
+        try {
+            String tulipHostName = botExecutionModel.getProposedBotInputs().get("tulipHostName").getValue().toString();
+            if (tulipHostName == null || tulipHostName.equals("")) {
+                throw new Exception("because the value of the key 'tulipHostName' is either null or an empty string");
+            }
+            hostUserAndPassword.put("hostName", tulipHostName);
+            info("host name has been added: " + tulipHostName);
+        } catch (NullPointerException nullPointerException) {
+            throw new NullPointerException("because the key 'tulipHostName' was not found in bot inputs " + nullPointerException.getMessage());
+        } catch (Exception exception) {
+            throw new Exception("while fetching inputs " + exception.getMessage() + " " + Arrays.toString(exception.getStackTrace()));
+        }
+        try {
+            List<BotIdentity> botIdentityList = botExecutionModel.getIdentityList();
+            if (botIdentityList.isEmpty()) {
+                throw new Exception("because no identities has been assigned to this bot kindly assign " + "the required identities");
+            }
+            Optional<BotIdentity> identity = botIdentityList.stream().filter(n -> (n.getIdentityType().name().equals("BASIC_AUTH") && n.getName().equals(genericIdentityKey))).findFirst();
+            if (identity.isPresent()) {
+                hostUserAndPassword.put("username", identity.get().getCredential().getBasicAuth().getUsername());
+                info("User name has been added : {username should not be printed} ");
+                hostUserAndPassword.put("password", identity.get().getCredential().getBasicAuth().getPassword());
+                info("password has been added : {password should never be printed} ");
+            } else {
+                throw new Exception("because an identity with key '" + genericIdentityKey + "' " + "was not found in identity list ");
+            }
+        } catch (Exception exception) {
+            throw new Exception("while fetching identity from identities list with the key: '" + genericIdentityKey + "' " + exception.getMessage() + " " + Arrays.toString(exception.getStackTrace()));
+        }
+        return hostUserAndPassword;
+    }
+}
