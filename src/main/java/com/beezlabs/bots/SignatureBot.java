3c568883d31684c171bb8dd43d4b358b4898baeb
Abishek Ayyappan <jettleetrif@gmail.com>
2023-05-02 14:26:36 +0530

    feat: add botLogic method

---
+++ src/main/java/com/beezlabs/bots/SignatureBot.java
@@ -0,0 +1,11 @@
+package com.beezlabs.bots;
+
+import com.beezlabs.hiveonserver.libs.JavaBotTemplate;
+import com.beezlabs.tulip.libs.models.BotExecutionModel;
+
+public class SignatureBot extends JavaBotTemplate {
+    @Override
+    protected void botLogic(BotExecutionModel botExecutionModel) {
+
+    }
+}
