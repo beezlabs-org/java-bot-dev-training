478f6ab8d05e1db48cf34483a870630ef5c6cb5d
Abishek Ayyappan <jettleetrif@gmail.com>
2023-05-02 15:56:40 +0530

    test commit

--- src/main/java/com/beezlabs/bots/SignatureBot.java
+++ src/main/java/com/beezlabs/bots/SignatureBot.java
@@ -3,9 +3,15 @@ package com.beezlabs.bots;
 import com.beezlabs.hiveonserver.libs.JavaBotTemplate;
 import com.beezlabs.tulip.libs.models.BotExecutionModel;
 
+import java.util.Arrays;
+
 public class SignatureBot extends JavaBotTemplate {
     @Override
     protected void botLogic(BotExecutionModel botExecutionModel) {
-
+        try {
+            success("SignatureBot has been executed successfully");
+        } catch (Exception exception) {
+            error("while running botLogic method " + exception.getMessage() + " " + Arrays.toString(exception.getStackTrace()));
+        }
     }
 }
